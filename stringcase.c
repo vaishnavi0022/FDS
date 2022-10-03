c75a149d32270a77a6737903ce27bd4d02d5f939
vaishnavi0022 <103585490+vaishnavi0022@users.noreply.github.com>
2022-10-03 13:35:18 +0530

    Add files via upload

---
+++ stringcase.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+ 
+int main()
+{
+  	char Str1[100];
+  	int i;
+ 
+  	printf("\n enter a string : ");
+  	gets(Str1);
+  	
+  	for (i = 0; Str1[i]!='\0'; i++)
+  	{
+  		if(Str1[i] >= 'a' && Str1[i] <= 'z')
+  		{
+  			Str1[i] = Str1[i] - 32;
+		}		
+  		else if(Str1[i] >= 'A' && Str1[i] <= 'Z')
+  		{
+  			Str1[i] = Str1[i] + 32;
+		}
+  	}
+
+  	printf("\n The Given String after changing the case = %s", Str1);
+  	
+  	return 0;
+
+}
