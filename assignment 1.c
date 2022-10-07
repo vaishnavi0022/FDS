e30095b9a3afcb6be50b669f4054b79bfc52d8c2
vaishnavi0022 <103585490+vaishnavi0022@users.noreply.github.com>
2022-10-07 15:12:40 +0530

    Update assignment 1.c

--- assignment 1.c
+++ assignment 1.c
@@ -100,3 +100,52 @@ printf("\n");
 
     return 0;
 }
+\*#include<stdio.h>
+int main()
+{
+    int a[3][3] ,min,max,i,j,s[2][2] ,k ;
+    printf("enter a number") ;
+    for(i=0;i<3;i++)
+    {  for(j=0;j<3;j++)
+   
+    {
+        scanf("%d",&a[i][j]) ;
+    }
+}
+ for(i=0;i<3;i++)
+    {  min=a[i][0] ;
+        for(j=0;j<3;j++)
+   {
+       if(min>=a[i][j])
+       {
+           min=a[i][j] ;
+           s[0][0] =i ;
+           s[0][1]= j;
+           
+        }
+                 
+   }
+       j=s[0] [1] ;
+       max=a[0] [j] ;
+       
+        for(k=0;k<3;k++)
+   {
+       if(max<=a[k][j])
+       {
+           max=a[k][j] ;
+           s[1][0] =k ;
+           s[1][1]= j;
+           
+        }
+                 
+   } 
+
+  if(min==max&&s[0][0]==s[1][0]&&s[0][1]==s[1][1])
+{
+    printf("%d",a[s[0][0] ][s[0][1]]) ;
+    
+}
+}
+return 0 ;
+}
+*\
